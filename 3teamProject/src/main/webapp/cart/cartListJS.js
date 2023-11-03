86363d66171d3b26d564435de804d3128f9a6aa8
jaman9753 <jongheon0009@gmail.com>
2023-11-03 17:36:43 +0900

    전체선택 구현

--- 3teamProject/src/main/webapp/cart/cartListJS.js
+++ 3teamProject/src/main/webapp/cart/cartListJS.js
@@ -1,3 +1,14 @@
-$(function(){
-	
+$(function() {
+	$("#CheckAll").click(function() {
+		if ($("#CheckAll").is(":checked")) $("input[name=cart_id]").prop("checked", true);
+		else $("input[name=cart_id]").prop("checked", false);
+	});
+
+	$("input[name=cart_id]").click(function() {
+		var total = $("input[name=cart_id]").length;
+		var checked = $("input[name=cart_id]:checked").length;
+
+		if (total != checked) $("#CheckAll").prop("checked", false);
+		else $("#CheckAll").prop("checked", true);
+	});
 });
